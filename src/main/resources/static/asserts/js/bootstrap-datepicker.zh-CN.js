ce13618ac469d55c99272956636fcf656a2eaac5
zhang_htao <1213462884@qq.com>
2019-08-01 08:36:15 +0800

    添加sqlite数据库文件

--- src/main/resources/static/asserts/js/bootstrap-datepicker.zh-CN.js
+++ src/main/resources/static/asserts/js/bootstrap-datepicker.zh-CN.js
@@ -3,7 +3,7 @@
  * Yuan Cheung <advanimal@gmail.com>
  */
 ;(function($){
-	$.fn.datetimepicker.dates['zh-CN'] = {
+	$.fn.datepicker.dates['zh-CN'] = {
 		days: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"],
 		daysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六"],
 		daysMin: ["日", "一", "二", "三", "四", "五", "六"],
